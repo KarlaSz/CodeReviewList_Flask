ceb0a53c0fcd1f6383a851bbd71f8f0770e5f890
KarlaSz <szymaszkiewiczk@gmail.com>
2025-03-25 21:49:28 +0100

    added error msg while adding task with the same name + flash msg

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from flask import Flask, render_template, request, redirect, url_for, jsonify, make_response
+from flask import Flask, render_template, request, redirect, url_for, jsonify, make_response, flash
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 import os
 
 app = Flask(__name__)
 
+#random (not for pdoruction, because we can lost user session)
+#Secret key for flashing messages (important for security)
+app.secret_key = os.urandom(24)
+
+#db setup
 basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{os.path.join(basedir, "todo.db")}'
 db = SQLAlchemy(app)
@@ -13,11 +18,11 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
 #object aplication to interactions
-#model, class which represent table
+#model, class which represent db table
 class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
-    priority = db.Column(db.String, nullable=False, default='low') #priority column
+    priority = db.Column(db.String, nullable=False) #priority column
 
     def to_dict(self):
         return {'id': self.id, 'name': self.name, 'priority': self.priority}
@@ -25,15 +30,27 @@ class Task(db.Model):
 #start app, get and show items
 @app.route('/', methods=['GET', 'POST'])
 def todo():
+    error_message = None
     if request.method == 'POST':
         task_name = request.form['task']
         task_priority = request.form['priority'] #getting priority from form
 
-        if task_name.strip():  # check if task is not empty
+        existing_task = Task.query.filter_by(name=task_name).first()
+
+        # check if task exist with the same name
+        if existing_task:
+            # Show flash message if task with the same name already exists
+            flash('Zadanie o tej nazwie już istnieje!', 'danger')
+            return redirect(url_for('todo'))  # redirect to same page
+
+        # check if task is not empty
+        if task_name.strip():
             task = Task(name=task_name, priority=task_priority)
             db.session.add(task)
             db.session.commit()
-        return redirect(url_for('todo')) #redirect to home page
+            return redirect(url_for('todo'))  # redirect to home page
+
+
 
     tasks = Task.query.all()
     return render_template('todo.html', tasks=tasks)
